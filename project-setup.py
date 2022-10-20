fd84ee728b9ca01c20d80cc134c9639090c2ee91
Richard <charichard09@gmail.com>
2022-10-20 08:13:49 -0700

    fix typo

--- project-setup.py
+++ project-setup.py
@@ -5,7 +5,7 @@ import pyperclip
 
 def mkProject():
   directory_name = input("What is the project name?\n")
-  print(f"Copied to clipboard:\nmkdir {directory_name} {directory_name}/ss {directory_name}/js\n")
+  print(f"Copied to clipboard:\nmkdir {directory_name} {directory_name}/css {directory_name}/js\n")
   pyperclip.copy(f"mkdir {directory_name} {directory_name}/css {directory_name}/js")
   input("Please press enter when finished.\n")
   print(f"Copied to clipboard:\ntouch index.html {directory_name}/css/styles.css {directory_name}/js/scripts.js")
