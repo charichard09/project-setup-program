7c907fd2ab23d8a5fa3904b29e0fb3e3e87f3a94
Richard <charichard09@gmail.com>
2022-10-20 16:26:19 -0700

    combined both lines to 1 line with pipe key

--- project-setup.py
+++ project-setup.py
@@ -5,11 +5,7 @@ import pyperclip
 
 def mkProject():
   directory_name = input("What is the project name?\n")
-  print(f"Copied to clipboard:\nmkdir {directory_name} {directory_name}/css {directory_name}/js\n")
-  pyperclip.copy(f"mkdir {directory_name} {directory_name}/css {directory_name}/js")
-  input("Please press enter when finished.\n")
-  print(f"Copied to clipboard:\ntouch index.html {directory_name}/css/styles.css {directory_name}/js/scripts.js")
-  pyperclip.copy(f"touch {directory_name}/README.md {directory_name}/index.html {directory_name}/css/styles.css {directory_name}/js/scripts.js")
-
+  print(f"Copied to clipboard:\nmkdir {directory_name} {directory_name}/css {directory_name}/js | touch {directory_name}/README.md {directory_name}/index.html {directory_name}/css/styles.css {directory_name}/js/scripts.js")
+  pyperclip.copy(f"mkdir {directory_name} {directory_name}/css {directory_name}/js | touch {directory_name}/README.md {directory_name}/index.html {directory_name}/css/styles.css {directory_name}/js/scripts.js")
 
 mkProject()
